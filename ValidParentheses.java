b598a044f6a65a3da9a619f4752e5b1e933c0d59
Super <79644813+Super3-Codes@users.noreply.github.com>
2022-10-16 21:14:24 +0300

    Create ValidParentheses.java

---
+++ ValidParentheses.java
@@ -0,0 +1,20 @@
+public boolean isValid(String s) {
+        
+        String[] str = s.split("");
+            
+        Stack<Character> stack = new Stack<>();
+    
+        for (char c : s.toCharArray()){
+            if(c == '('){
+                stack.push(')');
+            }else if(c == '['){
+                stack.push(']');
+            }else if(c == '{'){
+                stack.push('}');
+            }else if(stack.isEmpty() || stack.pop() != c){
+                return false;
+            }
+        }
+        return stack.isEmpty();
+        
+    }
