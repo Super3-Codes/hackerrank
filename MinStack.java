40e310652b88b1da84d6fd6b33dd76172a4a0a39
Super <79644813+Super3-Codes@users.noreply.github.com>
2022-10-17 08:27:47 +0300

    Create MinStack.java

---
+++ MinStack.java
@@ -0,0 +1,34 @@
+class MinStack {
+
+    int min = Integer.MAX_VALUE;
+    Stack<Integer> stack = new Stack<Integer>();
+    
+    
+    public MinStack() {
+        
+    }
+    
+    public void push(int val) {
+        if(val <= min){
+            stack.push(min);
+            min = val;
+        }
+        stack.push(val);
+        
+    }
+    
+    public void pop() {
+        if(stack.pop() == min){
+            min = stack.pop();
+        }
+    }
+    
+    public int top() {
+        return stack.peek();
+        
+    }
+    
+    public int getMin() {
+        return min;
+    }
+}
