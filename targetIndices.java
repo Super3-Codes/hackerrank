a4f877aafacb4e9d522d8df542edb43b62fc8d32
Super <79644813+Super3-Codes@users.noreply.github.com>
2022-10-19 23:12:00 +0300

    Create targetIndices.java

---
+++ targetIndices.java
@@ -0,0 +1,16 @@
+  public List<Integer> targetIndices(int[] nums, int target) {
+        int smaller = 0;
+        int equal = 0;
+        for (int num : nums) {
+            if (num < target) {
+                smaller++;
+            } else if (num == target) {
+                equal++;
+            }
+        }
+		List<Integer> indices = new ArrayList<>(equal);
+        for (int i = smaller; i < smaller + equal; i++) {
+            indices.add(i);
+        }
+        return indices;
+    }
