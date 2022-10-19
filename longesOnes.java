53df566e81a54ce44d534ea87d5482c97a2f721e
Super <79644813+Super3-Codes@users.noreply.github.com>
2022-10-19 15:48:16 +0300

    Create longesOnes.java

---
+++ longesOnes.java
@@ -0,0 +1,20 @@
+ public int longestOnes(int[] nums, int k) {
+        int oneCount = 0;
+        int zeroCount = 0;
+        
+        int left = 0;
+        int max=0;
+        for(int right = 0 ; right < nums.length ; right++ ){
+            if(nums[right]==0)zeroCount++;
+            else oneCount++;
+            
+            while(zeroCount>k){
+                if(nums[left]==0)zeroCount--;
+                else oneCount--;
+                left++;
+            }
+            
+            max=Math.max(right-left+1,max);
+        }
+        return max;
+    }
